596db95a2b07b9b3e672b3cbc1b50d1721ec6c9a
Leo Komissarov <komissarov@scm.com>
2021-02-25 14:08:50 +0100

    add str method to Mol

--- molgan/mol.py
+++ molgan/mol.py
@@ -2,15 +2,25 @@ import numpy as np
 import hashlib
 
 class Mol:
-    def __init__(self, xyz, numbers):
+    def __init__(self, xyz, numbers, name=None):
         assert len(numbers) == len(xyz)
         self.numbers = np.array(numbers)
         self.xyz     = np.array(xyz)
         assert self.xyz.ndim == 2
         assert self.xyz.shape[-1] == 3
+        self.name = name
 
     def __len__(self):
         return len(self.xyz)
+
+    def __str__(self):
+        s = ''
+        for num,coords in zip(self.numbers, self.xyz):
+            _  = ''.join(f"{i:>10.6f}" for i in coords)
+            s += f"{num:<4}{_}\n"
+        return s
+
+
 
     def dm(self, round=None) -> np.ndarray:
         """ Return the distance matrix of self """
